f8ee84e50dce0695fd0e047ca0e4832cfc573306
muktharayodeji6-a11y <muktharayodeji6@gmail.com>
2026-07-09 01:52:09 +0100

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,1 +1,14 @@
+const USERNAME = "admin";
+const PASSWORD = "joker123";
 
+function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === USERNAME && password === PASSWORD) {
+        localStorage.setItem("loggedIn", "true");
+        window.location.href = "dashboard.html";
+    } else {
+        alert("Invalid username or password");
+    }
+}
